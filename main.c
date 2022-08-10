ecb87ffcd33c4b5caf2289522e857925d76d353f
abelasfaw <abeloman89@gmail.com>
2022-08-10 13:16:37 -0700

    added main

---
+++ main.c
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <limits.h>
+void _isterminal(void)
+{
+	if(isatty(STDIN_FILENO))
+	{
+		_puts("$ ");
+	}
+}
+int main(int argc, char *args[], char *env[])
+{
+	char *input_buffer;
+	size_t buffer_size;
+	int status, words_count, path_status;
+	char *argv[100];
+	char *updated_path;
+	/**pid_t child1;**/
+	words *head;
+
+	head = NULL;
+	buffer_size = 32;
+	input_buffer = malloc(sizeof(char) * buffer_size);
+	if (input_buffer == NULL)
+	{
+		_puts("Malloc error \n");
+		/**printf("Malloc error \n");**/
+		return (-1);
+	}
+	while (1)
+	{
+		_isterminal();
+		status = read_input(input_buffer, buffer_size);
+		if (status == -1)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				_puts("\n");
+			}
+			exit(1);
+		}
+		words_count = split_string(input_buffer, &head);
+		if (words_count == -1)
+		{
+			exit(1);
+		}
+		if (build_argument_vector(head, argv) == -1)
+		{
+			exit(1);
+		}
+		if (argv[0] == NULL)
+		{
+			head = NULL;
+			continue;
+		}
+		if (check_command(argv[0]) == 0)
+		{
+			head = NULL;
+			continue;
+		}
+		/**printf("argv[0]: %s\n", argv[0]);**/
+		head = NULL;
+		path_status = check_command_path(argv[0]);
+		if (path_status == -1)
+		{
+			perror(args[argc - argc]);
+			continue;
+		}
+		if (path_status == 1)
+		{
+			updated_path = create_complete_path(argv[0]);
+			execute(updated_path, argv, env);
+		}
+		else
+		{
+			execute(argv[0], argv, env);
+		}
+	}
+	return (0);
+}
